48030605dd671dd0597b1a2eac2f839c06ec7a43
Alberto Benatti <albi.benni8@gmail.com>
2025-09-06 14:34:39 +0200

    refactor in multi file - parsers

---
+++ parser/parse-note-by-line.go
@@ -0,0 +1,36 @@
+package parser
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+func checkNotesByTitle(title string, buffLine string, isNextNote bool) (result string, isNextNotee bool, err error) {
+	switch {
+	// taking in consideration cases where == lines are duplicates so isNextNote is true - not considered
+	// and return true already
+	case isNextNote:
+		titleTrimmed := strings.TrimSpace(title)
+		if len(titleTrimmed) == 0 {
+			return "", false, errors.New("Title not defined")
+		}
+		if strings.Contains(buffLine, titleTrimmed) {
+			fmt.Printf("Found new note of Title %s\n", titleTrimmed)
+			return "", false, nil
+		}
+		return "", isNextNote, nil
+	case buffLine == "==========\r", buffLine == "==========\n", buffLine == "==========", buffLine == "==========\r\n":
+		fmt.Printf("delimiter not counted - new note coming")
+		return "", true, nil
+	case len(strings.TrimSpace(buffLine)) == 0:
+		fmt.Println("Empty note line")
+		return "", isNextNote, nil
+
+	case strings.Contains(strings.ToLower(buffLine), "your highlight "):
+		fmt.Println("Skipping boilerplate lane - your hightlight...")
+		return "", isNextNote, nil
+	default:
+		return buffLine, isNextNote, nil
+	}
+}
